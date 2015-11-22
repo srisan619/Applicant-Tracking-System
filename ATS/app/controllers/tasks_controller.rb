e6a97cc30e4db6923c2f5012c502091960a71c40
srinivasan <srisan619@gmail.com>
2015-11-22 15:29:50 +0530

    Edit & destroy task is completed

--- ATS/app/controllers/tasks_controller.rb
+++ ATS/app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   layout 'employer'
   before_filter :require_login
+  before_action :set_task,  only: [:edit, :show, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -15,6 +16,33 @@ class TasksController < ApplicationController
     if @task.save
       flash[:notice] = 'Task has been created successfully.'
       redirect_to tasks_path
+    else
+      flash[:alert] = '#{@task.errors.full_messages.join(' , ')}'
+      redirect_to new_task_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:notice] = 'Task has been updated successfully.'
+      redirect_to tasks_path
+    else
+      flash[:alert] = '#{@task.errors.full_messages.join(' , ')}'
+      redirect_to edit_task_path(@task)
+    end
+  end
+
+  def destroy
+    if @task.destroy
+      flash[:notice] = 'Task has been updated successfully.'
+      redirect_to tasks_path
+    else
+      flash[:alert] = '#{@task.errors.full_messages.join(' , ')}'
+      redirect_to tasks_path
     end
   end
 
@@ -22,5 +50,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:task_category_id, :user_id, :is_email_notify, :subject, :task_date, :start_time, :end_time )
+  end
+
+  def set_task
+    @task = Task.where(id: params[:id]).first
   end
 end
